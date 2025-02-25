1eb693e449cf91288495e8739435d123c6851ab1
Ayeshaa-Taahir <ayesha.tahir.dhanoooo@gmail.com>
2025-02-25 22:29:11 +0500

    variable and datatype in java

---
+++ VariableDataType.java
@@ -0,0 +1,11 @@
+public class VariableDataType {
+
+    public static void main(String[] args) {
+        System.out.println("** Variable and Data in Java **");
+        //decleration
+        int age;
+        //initialization
+        age = 10;
+        System.out.println(age);
+    }
+}
