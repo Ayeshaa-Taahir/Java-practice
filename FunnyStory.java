3f911e1710299a87743f7e66ba80e0397f8773b4
Ayeshaa-Taahir <ayesha.tahir.dhanoooo@gmail.com>
2025-03-17 00:07:17 +0500

    Random story generator

---
+++ FunnyStory.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class FunnyStory {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        String[] characters = {"Ayesha", "Manal", "Bakhtawar", "Areeba", "Reja","Faiqa"};
+        String[] places = {"a magical forest", "a haunted house", "a compter lab", "a secret cave", "a mysterious library"};
+        String[] actions = {"discovered a hidden treasure", "annoy eachother", "built a time machine", "found an alien", "became a superhero"};
+        String[] twists = {"but suddenly disappeared", "but it was all a dream", "and saved the world", "and accidentally started a time loop", "and met a talking cat"};
+        String character = characters[random.nextInt(characters.length)];
+        String place = places[random.nextInt(places.length)];
+        String action = actions[random.nextInt(actions.length)];
+        String twist = twists[random.nextInt(twists.length)];
+       System.out.println("\n Here is your random funny story:\n");
+        System.out.println(character + " went to " + place + " and " + action + ", " + twist + "!");
+
+        System.out.println("\n The End! ");
+    }
+}
