43f2dfb199c01a17c558d7cbf475b351fca873e1
Ayeshaa-Taahir <ayesha.tahir.dhanoooo@gmail.com>
2025-03-17 00:15:56 +0500

    change in places

--- FunnyStory.java
+++ FunnyStory.java
@@ -6,8 +6,8 @@ public class FunnyStory {
 
         String[] characters = {"Ayesha", "Manal", "Bakhtawar", "Areeba", "Reja","Faiqa"};
         String[] places = {"a magical forest", "a haunted house", "a compter lab", "a secret cave", "a mysterious library"};
-        String[] actions = {"discovered a hidden treasure", "annoy eachother", "built a time machine", "found an alien", "became a superhero"};
-        String[] twists = {"but suddenly disappeared", "but it was all a dream", "and saved the world", "and accidentally started a time loop", "and met a talking cat"};
+        String[] actions = {"discovered a secret relationship", "annoy eachother", "built a university", "found an puppy", "became a Doremon"};
+        String[] twists = {"between a princess and a driver", "but it was all a dream", "and saved the world", "and accidentally fall in love", "and met a talking cat"};
         String character = characters[random.nextInt(characters.length)];
         String place = places[random.nextInt(places.length)];
         String action = actions[random.nextInt(actions.length)];
